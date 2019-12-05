e092b5cb5472917256a35ba89d8ce05f8c0693b6
Rahavi Ramesh <RRamesh@spartaglobal.com>
2019-12-05 21:09:56 +0000

    Added monster class

---
+++ Monstropolis.py
@@ -0,0 +1,6 @@
+from monster_class import *
+
+Monster1 = Monster('Sulley','creating chaos')
+Monster2 = Monster('Mike', 'funny')
+
+print(Monster1)
---
+++ monster_class.py
@@ -0,0 +1,14 @@
+
+
+class Monster():
+    def __init__(Self, name, skills=[]):
+        Self.name = name
+        Self.skills = skills
+
+
+    def sleep(self):
+        return 'ZZzzzz'
+    def eat(self):
+        return 'Foooood...NOM NOM NOM'
+    def scare_attack(self):
+        return 'RAAAHHH'
